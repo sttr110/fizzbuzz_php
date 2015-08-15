20ef4807e349fde28c4be31b34ce72438d1a0902
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2015-08-15 07:29:31 +0000

    fizzbuzz実装

--- index.php
+++ index.php
@@ -2,14 +2,25 @@
 
 class Fizzbuzz
 {
-  const $FIZZ = 3;
-  const $BUZZ = 5;
+  const FIZZ = 3;
+  const BUZZ = 5;
 
   function exec($num) {
-    echo "hello $num";
+    for($i = 1; $i < $num; $i++) {
+      if(($i % self::FIZZ == 0) && ($i % self::BUZZ == 0)) {
+        echo "fizzbuzz<br>";
+      } else if($i % self::FIZZ == 0) {
+        echo "fizz<br>";
+      } else if($i % self::BUZZ == 0) {
+        echo "buzz<br>";
+      } else {
+        echo "$i<br>";
+      }
+    }
   }
+
 }
 
 
 $fb = new Fizzbuzz();
-$fb.exec(20);
+$fb->exec(20);
